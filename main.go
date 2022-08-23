8cfa3ceed7114e482ffd0f0ffcb5458ff5ec48aa
Tor Smedberg <tor.smedberg@gmail.com>
2022-08-23 09:43:15 +0200

    test

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"te4-introprojekt/metastore"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+	var i = metastore.CreateInformation()
+	fmt.Printf(i.GetCreated().String())
+}
